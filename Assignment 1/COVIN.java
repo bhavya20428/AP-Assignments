60b049ff505941cbb8eb358a4c4faf1f5c2e4c1f
bhavya20428 <bhavya20428@iiitd.ac.in>
2021-10-08 01:21:13 +0530

    Added classes

---
+++ Assignment 1/COVIN.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class COVIN{
+	public static void main(String[] args) {
+		
+	}
+}
+
+class Menu{
+	
+}
+
+
+class Citizen{
+
+}
+
+class Vaccine{
+
+}
+
+class Hospital{
+
+}
+
+class Slot{
+
+}
+
