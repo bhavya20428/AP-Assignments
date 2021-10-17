e6796caac1737f954b9fe802c3929cf6201c3a85
bhavya20428 <bhavya20428@iiitd.ac.in>
2021-10-18 02:26:27 +0530

    Added classes and interfaces

--- Assignment 2/BACKPACK.java
+++ Assignment 2/BACKPACK.java
@@ -2,8 +2,43 @@ import java.uitl.*;
 
 public class BACKPACK{
 	Scanner sc= new Scanner(System.in);
-	
 
+}
+
+class Course{
+
+}
+
+class Instructor{
+
+}
+
+class Student{
+
+}
+
+interface ClassMaterial{
+
+}
+
+class Slides{
+
+}
+
+class Videos{
+
+}
+
+interface Assessment{
+
+}
+
+
+class Quiz{
+
+}
+
+class Assignment{
 
 }
 
